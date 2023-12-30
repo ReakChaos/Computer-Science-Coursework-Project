4ad8b043ab97705b3e1b39b5f77e8970a8af3807
ReakChaos <17djayagoda@bishopshalt.school>
2023-12-30 21:33:00 +0000

    Added career button functionality
    Can now click on all the buttons showing the panels which will soon contain the races.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,18 +6,76 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] racePanels;
+    private bool clicked = false; // variable checking if a race button has been already pressed
+    private int panelNumber; // stores last panel number displayed
+    public GameObject defaultNav;
+    public GameObject onRaceNav;
+
+    // when a race button is clicked
+    public void RaceButtonClicked()
     {
-        Button raceButton = GetComponent<Button>();
-        if (raceButton)
-        {
-            raceButton.onClick.AddListener(ButtonClicked);
-        }
+        defaultNav.SetActive(false);
+        onRaceNav.SetActive(true);
+        clicked = true;
+
+    }
+    
+    // when the back button is pressed
+    public void BackButtonClicked()
+    {
+        // reset the clicked variable/navigation and close panels
+        clicked = false;
+        racePanels[panelNumber].SetActive(false);
+        defaultNav.SetActive(true);
+        onRaceNav.SetActive(false);
     }
 
-    void ButtonClicked()
+    public void RaceButton1()
     {
-        Debug.Log("Clicekd button: " + gameObject.name);
+        // if any button has been clicked before disable previous panel
+        if (clicked)
+        {
+            racePanels[panelNumber].SetActive(false);
+        }
+        // and open current panel
+        racePanels[0].SetActive(true);
+        panelNumber = 0;
+    }
+    public void RaceButton2()
+    {
+        if (clicked)
+        {
+            racePanels[panelNumber].SetActive(false);
+        }
+        racePanels[1].SetActive(true);
+        panelNumber = 1;
+    }
+    public void RaceButton3()
+    {
+        if (clicked)
+        {
+            racePanels[panelNumber].SetActive(false);
+        }
+        racePanels[2].SetActive(true);
+        panelNumber = 2;
+    }
+    public void RaceButton4()
+    {
+        if (clicked)
+        {
+            racePanels[panelNumber].SetActive(false);
+        }
+        racePanels[3].SetActive(true);
+        panelNumber = 3;
+    }
+    public void RaceButton5()
+    {
+        if (clicked)
+        {
+            racePanels[panelNumber].SetActive(false);
+        }
+        racePanels[4].SetActive(true);
+        panelNumber = 4;
     }
 }
